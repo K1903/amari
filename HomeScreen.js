05618e7c3c997eeae3111b66c4ce50b1a7d77955
ColeBielby <colebielby@gmail.com>
2023-11-24 14:59:29 -0600

    Added home button to camera screen series

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,14 +1,23 @@
 // HomeScreen.js
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Button, Text } from 'react-native';
+import ScreenContext from './Contexts/ScreenContext';
+import { TouchableHighlight } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
+  const [screen, setScreen] = useContext(ScreenContext);
   return (
-    <View style={{backgroundColor:"white", marginTop:100}}>
+    <View style={{backgroundColor:"white", flex:1}}>
       <Button
         title="Take a Photo"
         onPress={() => navigation.navigate('Camera')}
       />
+      <TouchableHighlight
+      onPress={() => setScreen("home")} 
+      underlayColor={"#bfbfbf"}
+      style={{alignItems:"center", justifyContent:"center", borderWidth:1.5, borderRadius:50, height:75, width:150, position:"absolute", bottom:"10%", alignSelf:"center"}}>
+                <Text style={{fontWeight:700, fontSize:16}}>Go Home</Text>
+      </TouchableHighlight>
     </View>
   );
 };
