a7351fbf2760b0989a6da964bec640bbcd1f60eb
Kaldan <34424662+K1903@users.noreply.github.com>
2023-12-12 10:38:01 -0600

    fixed clothing

---
+++ Clothing.js
@@ -0,0 +1,15 @@
+class Clothing {
+    Type;
+    Season;
+    Name;
+    Image;
+    constructor(Type, Season, Name, Image) {
+        this.Type = Type;
+        this.Season = Season;
+        this.Name = Name;
+        this.Image = Image
+    }
+
+}
+
+export default Clothing;
