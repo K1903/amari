116337b5b3af23f0feb7a166c7824d327b9533ec
ColeBielby <colebielby@gmail.com>
2023-12-11 18:37:15 -0600

    Added delete method for clothing items

--- Screens/Closet.jsx
+++ Screens/Closet.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Image, Modal, ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 import { BottomThreeButtons } from '../BottomThreeButtons';
 import ClothingStorage from '../ClothingStorage';
 import ScreenContext from '../Contexts/ScreenContext';
@@ -8,6 +8,9 @@ import { useContext } from 'react';
 function Closet(props) {
     const [loadedArray, setLoadedArray] = useState([]);
     const [screen, setScreen] = useContext(ScreenContext);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [itemToDel, setItemToDel] = useState(null);
+    const clothingStorage = new ClothingStorage();
     
 
     useEffect( () => {
@@ -15,7 +18,6 @@ function Closet(props) {
             try {
                 const clothingStorage = new ClothingStorage();
                 const data = await clothingStorage.loadClothingArray();
-                console.log(data);
                 setLoadedArray(data || []);
             } catch (error) {
                 console.error('Error:', error);
@@ -33,14 +35,15 @@ function Closet(props) {
         },
         horizScroll: {
             height: 200,
+            marginBottom: 10
         },
         lastHorizScroll: {
             height: 200,
-            paddingVertical: 200,
+            marginBottom:250
         },
         imageStyle: {
-            width: 100, // Adjust the width as needed
-            height: 100, // Adjust the height as needed
+            width: 200, // Adjust the width as needed
+            height: 200, // Adjust the height as needed
             marginHorizontal: 5,
         },
     });
@@ -48,17 +51,39 @@ function Closet(props) {
     const renderCategory = (category, images) => (
         <View key={category}>
             <Text style={styles.headerText}>{category}</Text>
-            <ScrollView horizontal={true} style={styles.horizScroll}>
+            <ScrollView horizontal={true} style={category === "Accessory" ? styles.lastHorizScroll : styles.horizScroll}>
                 {images.map((image, index) => (
-                    <Image key={index} source={{ uri: image.Image.uri }} style={styles.imageStyle} />
+                    <TouchableOpacity key={index} onLongPress={() => openModal(image)}>
+                        <Image source={{ uri: image.Image.uri }} style={styles.imageStyle} />
+                    </TouchableOpacity>
+                    
                 ))}
             </ScrollView>
         </View>
     );
 
+    function openModal(item) {
+        setModalVisible(true);
+        setItemToDel(item);
+    }
+
+    function closeModal() {
+        setModalVisible(false);
+        setItemToDel(null);
+    }
+
+    async function deleteItem() {
+        await clothingStorage.loadClothingArray();
+        await clothingStorage.remove(itemToDel);
+        const data = await clothingStorage.loadClothingArray();
+        setLoadedArray(data || []);
+        closeModal();
+        alert("Item successfully deleted")
+    }
+
     return (
-        <View style={{ backgroundColor: 'white' }}>
-            <ScrollView style={{ paddingTop: 100 }}>
+        <View style={{backgroundColor: "white"}}>
+            <ScrollView style={{paddingTop: 100}}>
                 {/* Render each category */}
                 {renderCategory('Belt', loadedArray.filter(item => item.Type === 'Belt' && item.Season.toLowerCase().includes(screen)))}
                 {renderCategory('Hat', loadedArray.filter(item => item.Type === 'Hat' && item.Season.toLowerCase().includes(screen)))}
@@ -68,6 +93,21 @@ function Closet(props) {
                 {renderCategory('Shoes', loadedArray.filter(item => item.Type === 'Shoes' && item.Season.toLowerCase().includes(screen)))}
                 {renderCategory('Accessory', loadedArray.filter(item => item.Type === 'Accessory' && item.Season.toLowerCase().includes(screen)))}
             </ScrollView>
+            <Modal visible={modalVisible}>
+            <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                    <View style={{backgroundColor: "white", padding: 20, borderRadius: 10}}>
+                        <Text style={{fontSize:18}}>Are you sure you want to delete this item?</Text>
+                        <View style={{flexDirection: "row", justifyContent: "space-around", marginTop: 35}}>
+                            <TouchableHighlight underlayColor={"#bfbfbf"} style={{borderWidth:1.5, width:120, height:80, justifyContent:"center", alignItems:"center"}} onPress={() => deleteItem()}>
+                                <Text style={{fontSize:20}}>Yes</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight underlayColor={"#bfbfbf"} style={{borderWidth:1.5, width:120, height:80, justifyContent:"center", alignItems:"center"}} onPress={() => closeModal()}>
+                                <Text style={{fontSize:20}}>No</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
             <View style={{ position: 'absolute', bottom: '5%', left: 0, right: 0 }}>
                 <BottomThreeButtons />
             </View>
